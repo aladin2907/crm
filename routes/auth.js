353bb9e4bb1d44ae218c0795bf537b2352ccd254
Vadim Nagornyi <uvn2203@gmail.com>
2019-04-28 16:33:29 +0300

    5

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const exspress = require('express')
 const router = exspress.Router()
 const controller = require('../controllers/auth')
     //localhost:5000/api/auth/login
-router.get('/login', controller.login)
+router.post('/login', controller.login)
 
 //localhost:5000/api/auth/register
-router.get('/register', controller.register)
+router.post('/register', controller.register)
 
 module.exports = router
