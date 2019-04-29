754d9696f8c7f8a8188c4f92c7846ce8fbc230af
Vadim Nagornyi <uvn2203@gmail.com>
2019-04-29 10:33:51 +0300

    6

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 //app.js
 const express = require('express')
+const bodyParser = require('body-parser')
 const analyticsRoutes = require('./routes/analytics')
 const authRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
-
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/auth', authRoutes)
