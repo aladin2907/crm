fd866a1cfb8743159fd552926bb50cc7bfaabf09
Vadim Nagornyi <uvn2203@gmail.com>
2019-05-14 16:50:31 +0300

    dbworking 2 part is all

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://platon:<Platon2203>@crm-1anow.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://platon:Platon2203@crm-1anow.mongodb.net/test?retryWrites=true'
 }
