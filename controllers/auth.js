c337d8aa034923183cfd59cae2f3cf041969b9f8
Vadim Nagornyi <uvn2203@gmail.com>
2019-05-21 19:08:03 +0300

    if block

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,20 +3,25 @@ const User = require('../models/User')
 
 module.exports.login = function(req, res) {
 
-    res.status(200).json({
+   res.status(200).json({
         login: {
             email: req.body.email,
             password: req.body.password
         }
-    })
+    }) 
 }
 
 
-module.exports.register = function(req, res) {
+module.exports.register = async function(req, res) {
 //email password
-   new user = new User({
-       email: req.body.email,
-       password: req.body.password
-   })
-   user.save().then(() => console.log('user created'))
+const candidate = await User.findOne({email:req.body.email}) 
+if (candidate){
+    //пользователь существует
+    res.status(409).json({
+        message: 'user exist, try another name or sign in'
+    })
+}  
+else {
+    // create user
+}
 }
