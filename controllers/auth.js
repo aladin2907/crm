63b65248810c5e9566a5da9fee28322bdcc6dcb8
Vadim Nagornyi <uvn2203@gmail.com>
2019-05-22 15:56:51 +0300

    lesson 3.3 good

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,11 @@
-const User = require('../models/User')
+ const bcrypt =require('bcryptjs')
+ const User = require('../models/User')
 
 
 module.exports.login = function(req, res) {
 
-   res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
-        }
-    }) 
+   //
+   
 }
 
 
@@ -22,6 +19,20 @@ if (candidate){
     })
 }  
 else {
-    // create user
+    // need create user
+    const salt=bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+        email: req.body.email,
+        password: bcrypt.hashSync(password, salt)
+    })
+    try{
+        await user.save()
+        res.status(201).json(user)
+    }  catch(e){
+        //error must was doing
+
+    }
+    
 }
 }
